7ad86697c51d2fc4fa766cb513d5c2f436a95ac9
Yasutoki Takamura <y_takamura@hep.s.kanazawa-u.ac.jp>
2025-01-08 21:14:35 +0900

    Change texts and programs

---
+++ programs/README.md
@@ -0,0 +1,17 @@
+# About the files
+  In this directly, there are data files and program files.
+  Please read this file not to confuse.
+## Programs
+  * calc_rge.c
+    * オリジナルのRGEを解くもの
+  * calc_toma2.c
+    * もともと作っていた適当なスケールにフェルミオンが加わって, bの値が変わるもの.
+    * もうちょっとアルゴリズムを柔軟に考えたら他のモデルにも応用できる?
+
+## Data files
+  * data_i.dat  SMゲージカップリング　消すな
+  * data_fi.dat なにか1つ入るとこれをとけばいいことになる.
+
+## Shell files
+  * plot_SM_RGE.sh
+    * data_i.dat をプロットする
---
+++ programs/calc_rge.c
@@ -0,0 +1,81 @@
+// ------------------------------------------
+// Solve for Gauge coupling 
+//   Standard Model version
+//
+// Last Modified: Jan 08 2025
+// Author: Yasutoki Takamura
+// email: y_takamura@hep.s.kanazawa-u.ac.jp
+// -----------------------------------------
+
+/* Note of math.h
+    Usage the value "pi": M_PI
+    Power the value: pow(a,b)=a^b
+*/
+
+#include<stdio.h>
+#include<math.h>
+#include<float.h>
+
+# define N 10000 // calculation size
+
+// main plogram
+int main(void){
+
+  FILE *fp;
+
+// physical values 
+  double g, a, b_in, b, b2, b_F;
+  double t, t_max, dt;
+  double mu, mu_max;
+  double mu_F;
+
+/* Table of Standard model
+  rge.g1.initialValue = 0.461425
+  rge.g2.initialValue = 0.65184
+  rge.g3.initialValue = 1.2143
+  energy scale = 91.876
+  cf. hep-ph 1306.6879
+*/
+
+// Initial condition
+  g      =  1.220;      // Coupling constant at Energy = mu
+  mu     =  91.876;     // Initial Energy Scale
+  b_in   =  -7.0;       // coefficient of one loop
+
+// Calculation setting
+
+  mu_max =  pow(10,20); // Calculation energy range
+
+// 200 Fermion added
+//  mu_F = 14.0*pow(10,3);
+//  b_F  = 20.0/3.0;
+//  Comment outed in order to calculate simple version
+
+
+// Main program ------------------
+  t =     log(mu);
+  t_max = log(mu_max);
+
+  dt    = t_max/N;
+  
+  a = pow(g,2.0)/(4.0*M_PI);
+  a = pow(a,-1);             // Taking inverse for calculation
+  b = b_in/(2*M_PI);
+//  b2 = b + b_F/(2*M_PI);
+
+  fp = fopen("data.dat","w");
+
+// Calculatin Differential Equation
+  printf("g= %f, b_in= %f\n", g, b_in);
+  for(t; t<t_max; t=t+dt){
+    fprintf(fp, "%f %f\n", exp(t), a);
+//    if(t>log(mu_F)){
+//     b = b2;
+//    }
+    a = a - (b*dt);
+  }
+
+  fclose(fp);
+  return 0;
+}
+// EOF

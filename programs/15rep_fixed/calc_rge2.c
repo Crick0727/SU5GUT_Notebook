02156c923cd69b6d3dffa980d4c59baae7682f1f
Yasutoki Takamura <y_takamura@hep.s.kanazawa-u.ac.jp>
2025-01-26 23:26:44 +0900

    Daily Commit

---
+++ programs/15rep_fixed/calc_rge2.c
@@ -0,0 +1,85 @@
+// ------------------------------------------
+// Solve for Gauge coupling 
+// Calculation for 15 representation Higgs
+//
+// Last Modified: Jan 26 2025
+// Author: Yasutoki Takamura
+// email: y_takamura@hep.s.kanazawa-u.ac.jp
+// -----------------------------------------
+
+/* Note of math.h
+    Usage the value "pi": M_PI
+    Power the value: pow(a,b)=a^b
+*/
+
+#include<stdio.h>
+#include<math.h>
+#include<float.h>
+
+# define N 10000 // calculation size
+
+// main plogram
+int main(void){
+
+  FILE *fp;
+
+// physical values 
+  double g, a, b_in, b, b2, b3, b_1, b_2;
+  double t, t_max, dt;
+  double mu, mu_max;
+  double mu_1, mu_2;
+
+/* Table of Standard model
+  rge.g1.initialValue = 0.461425   b_1 = 41.0/10.0
+  rge.g2.initialValue = 0.65184    b_2 = -19.0/6.0
+  rge.g3.initialValue = 1.2143     b_3 = -7.0
+  energy scale = 91.876
+  cf. hep-ph 1306.6879
+*/
+
+// Initial condition
+  g      =  0.65184;      // Coupling constant at Energy = mu
+  mu     =  91.876;        // Initial Energy Scale [GeV]
+  b_in   =  -19.0/6.0;           // coefficient of one loop
+
+// Calculation setting
+
+  mu_max =  pow(10,20); // Calculation energy range
+
+//  Intermediate Particle
+  mu_1 = 250; // The Scale where the particle added [GeV]
+  b_1  = 1.0/2.0; // Value of Additional b value
+
+  mu_2 = 1540; // The Scale where the particle added [GeV]
+  b_2  = 2.0/3.0; // Value of Additional b value
+
+// Main program ------------------
+  t =     log(mu);
+  t_max = log(mu_max);
+
+  dt    = t_max/N;
+  
+  a = pow(g,2.0)/(4.0*M_PI);
+  a = pow(a,-1);             // Taking inverse for calculation
+  b = b_in/(2*M_PI);
+  b2 = b + b_1/(2*M_PI);
+  b3 = b2 + b_1/(2*M_PI);
+
+  fp = fopen("data.dat","w");
+
+// Calculatin Differential Equation
+  printf("g= %f, b_in= %f\n", g, b_in);
+  for(t; t<t_max; t=t+dt){
+    fprintf(fp, "%f %f\n", exp(t), a);
+    if(t>log(mu_1) && log(mu_2)>t ){
+      b = b2;
+    }
+    if(t>log(mu_2)){
+      b = b3;
+    }
+    a = a - (b*dt);
+  }
+  fclose(fp);
+  return 0;
+}
+// EOF
